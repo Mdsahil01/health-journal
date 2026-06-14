0e48eea228ee7f7da37777b6ec9b0e7f7b687e30
Mdsahil01 <mohammedsahil3752@gmail.com>
2026-06-15 01:52:40 +0530

    Added images,icons, and app branding

--- app/src/main/java/com/example/health_journal/MainActivity.kt
+++ app/src/main/java/com/example/health_journal/MainActivity.kt
@@ -4,18 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -28,7 +35,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.motionEventSpy
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.health_journal.ui.theme.HealthjournalTheme
@@ -52,9 +62,32 @@ fun home() {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(25.dp)
+            .padding(35.dp),
+        horizontalAlignment =
+            Alignment.CenterHorizontally
 
     ) {
+
+            Image(
+                painter = painterResource(
+                    R.drawable.health_journal_logo),
+                contentDescription = "Health journal Logo",
+                modifier = Modifier.size(120.dp)
+
+            )
+
+
+            Text(
+                text = "Health Journal"
+            )
+
+
+
+
+
+
+
+
         Text(
             text = "Welcome to Health Journal",
             modifier = Modifier
@@ -119,7 +152,12 @@ fun home() {
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.End
         ) {
-            Text(text = "Total Symptoms: ${symptoms.size}")
+                Text(text = "🩺 ")
+
+                Text(
+                    text = "Last Saved Symptom: $savedSymptom"
+                )
+
 
         }
     }
